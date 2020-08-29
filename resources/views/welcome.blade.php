607b4b7a44070b5d8ca0c7200316b6e238280374
Ajay Banstola <ajay.banstola@gmail.com>
2020-08-29 08:43:12 +0545

    margin lessened and button text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -292,7 +292,7 @@
             <div>
             <!-- <a class="navbar-brand" href="#">Covid19 Chart</a> -->
             <!-- <a href="{{ route('fundReceive') }}">Ask Fund</a> -->
-            <button class = "btn1 btn-lg btn-primary" onclick = "location.href = '{{ route('regD') }}'">Ask fund</button>
+            <button class = "btn1 btn-lg btn-primary" onclick = "location.href = '{{ route('regD') }}'">Request fund</button>
             <!-- <a href="{{ route('registerAsDonor') }}">Donate</a> -->
             <button class = "btn1 btn-lg btn-primary" onclick = "location.href = '{{ route('regR') }}'">Donate</button>
             <button class = "btn1 btn-lg btn-success" onclick = "location.href = '{{ route('login') }}'">Login</button>
